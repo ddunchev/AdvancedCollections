d2b6d03fbe22bfb54c4cae2e4e4affc5e031f00c
ddunchev <danchodunchev@gmail.com>
2017-05-08 13:07:59 +0300

    Add project files.

---
+++ AverageStudentGrades/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader(@"E:\input.txt"));
+#endif
+
+            var input = int.Parse(Console.ReadLine());
+
+            var grades = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < input; i++)
+            {
+                string[] read = Console.ReadLine().Split().ToArray();
+                string name = read[0];
+                double grade = float.Parse(read[1]);
+                if (!grades.ContainsKey(name)) grades[name] = new List<string>();
+                grades[name].Add(String.Format("{0:0.00}", grade));
+            }
+
+            foreach (var item in grades)
+            {
+                Console.WriteLine($"{item.Key} -> {String.Join(" ", item.Value)} (avg: {item.Value.Select(x => double.Parse(x)).ToList().Average():0.00})");
+
+            }
+
+        }
+    }
+}
